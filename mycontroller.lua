48bce354fae49a73f27b8398027de6689f1aeaa1
Sambuzi <sajmir.buzi@studio.unibo.it>
2026-05-21 21:55:52 +0200

    impoved code strenght and readability by using constants and removing redundant comments. Also added a comment to explain the logic of moving towards the brighter side.

--- mycontroller.lua
+++ mycontroller.lua
@@ -1,7 +1,11 @@
+--constant for my code (trying to make it more readable)
 MAX_VELOCITY = 15
 FORWARD = 10
-TURN = 6
+TURN = 5
 SLOW = 2
+
+LIGHT_REACHED = 2.5
+OBSTACLE_CLOSE = 0.15
 
 function init()
     robot.leds.set_all_colors("green")
@@ -10,40 +14,55 @@ end
 function step()
     local left, right = FORWARD, FORWARD
 
-    local prox_left = 0
-    local prox_right = 0
+    local front_prox = 0
+    local left_prox = 0
+    local right_prox = 0
+    local light_total = 0
     local light_left = 0
     local light_right = 0
 
-    for i = 1, 3 do
-        prox_left = prox_left + robot.proximity[i].value
-        light_left = light_left + robot.light[i].value
+    for i = 1, 24 do
+        local light = robot.light[i].value
+        light_total = light_total + light
+        if i <= 12 then
+            light_left = light_left + light
+        else
+            light_right = light_right + light
+        end
     end
 
-    for i = 22, 24 do
-        prox_right = prox_right + robot.proximity[i].value
-        light_right = light_right + robot.light[i].value
+    for i = 1, 4 do
+        left_prox = left_prox + robot.proximity[i].value
     end
+    for i = 21, 24 do
+        right_prox = right_prox + robot.proximity[i].value
+    end
+    front_prox = left_prox + right_prox
 
-    -- ostacolo molto vicino: evita
-    if prox_left + prox_right > 0.15 then
-        if prox_left > prox_right then
+    -- Stop when the robot is close to the light.
+    if light_total > LIGHT_REACHED then
+        left, right = 0, 0
+
+    -- Avoid obstacles in front of the robot.
+    elseif front_prox > OBSTACLE_CLOSE then
+        if left_prox > right_prox then
             left, right = FORWARD, SLOW
         else
             left, right = SLOW, FORWARD
         end
 
-    -- vicino alla luce: fermati
-    elseif light_left + light_right > 2.5 then
-        left, right = 0, 0
-
-    -- appena vede più luce a sinistra, gira a sinistra
-    elseif light_left > light_right + 0.01 then
+    -- Move towards the brighter side. Same as obs avoidance.
+    elseif light_left > light_right + 0.05 then
         left, right = TURN, FORWARD
-
-    -- appena vede più luce a destra, gira a destra
-    elseif light_right > light_left + 0.01 then
+    elseif light_right > light_left + 0.05 then
         left, right = FORWARD, TURN
+    end
+
+    if left > MAX_VELOCITY then
+        left = MAX_VELOCITY
+    end
+    if right > MAX_VELOCITY then
+        right = MAX_VELOCITY
     end
 
     robot.wheels.set_velocity(left, right)
